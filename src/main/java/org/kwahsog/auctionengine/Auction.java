b388017e641c0b050ad2f0cc2eed69c41c9b5040
Alex <cooper.alexander@gmail.com>
2017-08-06 17:29:11 +0100

    Add Auction interface and implementation

---
+++ src/main/java/org/kwahsog/auctionengine/Auction.java
@@ -0,0 +1,62 @@
+package org.kwahsog.auctionengine;
+
+import java.util.LinkedList;
+
+/**
+ * Implementation of the AuctionableItem interface. 
+ * Provides constuctor for an Auction on a given item, allowing bids to be made and bid information to be retrieved.
+ * @author Alex
+ *
+ */
+public class Auction implements AuctionableItem {
+	
+	private LinkedList<Bid> bids;	
+	private final int id;
+	private final Item item;
+	
+	/**
+	 * Creates an auction for a given item;
+	 * @param id
+	 * @param item
+	 */
+	public Auction(int id, Item item) {
+		super();
+		this.id = id;
+		this.item = item;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+	
+	public BidResult makeBid(Bid myBid) {
+		if (bids.size() > 0) {
+			if (bids.getLast().getPrice() < myBid.getPrice()) {
+				bids.add(myBid);
+				return BidResult.BID_SUCCESSFUL;
+			} else {
+				return BidResult.BID_TOO_LOW;
+			}
+		} else {
+			bids.add(myBid);
+			return BidResult.BID_SUCCESSFUL;
+		}
+	}
+
+	public Bid getWinningBid() {
+		if (bids.size() > 0) {
+			return bids.getLast();
+		} else {
+			return null;
+		}
+	}
+
+	public LinkedList<Bid> getAllBids() {
+		return bids;
+	}
+
+}
---
+++ src/main/java/org/kwahsog/auctionengine/AuctionableItem.java
@@ -0,0 +1,30 @@
+package org.kwahsog.auctionengine;
+
+import java.util.LinkedList;
+
+public interface AuctionableItem {
+	
+	public enum BidResult { BID_SUCCESSFUL, AUCTION_ENDED, BID_TOO_LOW};
+	
+	/** 
+	 * Records a user's bid on an item as part of an auction, evaluating success of the bid.
+	 * 
+	 * @param myBid Instance of the bid.
+	 * @return BidResult enum based on success of bid.
+	 */
+	public BidResult makeBid(Bid myBid); 
+	
+	/** 
+	 * Returns the current winning bid for an item in an auction.
+	 * 
+	 * @return Bid the winning bid.
+	 */
+	public Bid getWinningBid();
+	
+	/**
+	 * Returns all bids for a specific item in an auction.
+	 * 
+	 * @return all bids.
+	 */
+	public LinkedList<Bid> getAllBids();
+}
